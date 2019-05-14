4d46146a25c44444320a4c9b73610bde81155dd8
MohaBenMoussa <faceclef@outlook.fr>
2019-05-14 15:13:21 +0200

    ajout du dossier img

---
+++ Img/tsx/personnagefille.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.3" name="personnagefille" tilewidth="32" tileheight="32" tilecount="96" columns="12">
+ <image source="../tileset/personnagefille.png" width="384" height="256"/>
+</tileset>
