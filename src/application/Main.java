e037b0979cc79b93b57b6aead796b1ce4a56458a
elodielay <elodie_lay@icloud.com>
2019-05-21 15:36:02 +0200

    erreurs git

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,8 +1,10 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 
 
@@ -10,7 +12,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
+			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("/vue/vueMap.fxml"));
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
--- src/controleur/controleur.java
+++ src/controleur/controleur.java
@@ -7,16 +7,40 @@ import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.input.KeyCode;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 
 public class controleur implements Initializable{
 	
-    @FXML
-    private Circle perso;
     
-    private boolean goRight, goLeft, goUp;
+    @FXML
+    private Rectangle inventor1;
+
+    @FXML
+    private Rectangle inventor2;
+
+    @FXML
+    private Rectangle inventor3;
+
+    @FXML
+    private Rectangle inventor4;
+
+    @FXML
+    private Rectangle inventor5;
+
+    @FXML
+    private ImageView pioche;
+    
+    @FXML
+    private ImageView perso;
+
+    
+/*    private boolean goRight, goLeft, goUp;
     
     @FXML
     void move(KeyEvent ke) {
@@ -66,6 +90,13 @@ public class controleur implements Initializable{
     	if(goLeft) perso.setLayoutX(perso.getLayoutX()-10);
     	
     	if(goUp) perso.setLayoutX(perso.getLayoutY()+10);
+    }
+    */
+    
+    @FXML
+    void prendrePioche(MouseEvent event) {
+    	Image i1 = new Image("/img/climb-1.png");
+    	perso.setImage(i1);
     }
 
 
